07a72e596c3ec563585e3abaa0e7a532748889cd
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-10-04 00:03:32 +0530

    Stripe Payment Gateway need to fix 😒

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,8 @@ import cors from "cors";
 import { dbConnection } from "./config/db.js";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/userRoutes.js";
-import productRouter from "./routes/productRoutes.js"
-import paymentRouter from "./routes/paymentRoutes.js"
+import productRouter from "./routes/productRoutes.js";
+import paymentRouter from "./routes/paymentRoutes.js";
 
 const app = express();
 dotenv.config({ path: ".env" });
@@ -19,13 +19,13 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: "10mb" }));     // convert user requested data into json formate
+app.use(express.json({ limit: "10mb" })); // convert user requested data into json formate
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
 app.use("/api/v1/user", userRouter);
-app.use("/api/v1/product", productRouter, paymentRouter);
+app.use("/api/v1/product", productRouter);
 app.use("/api/v1", paymentRouter);
 
 dbConnection();
