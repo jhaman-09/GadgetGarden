0b3e52689818663f4d99790bff0650f301c5f506
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-10-23 09:25:07 +0530

    Cors error in frontend😒😒

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,7 @@ app.use(
   cors({
     origin: [process.env.FRONTEND_URL],
     methods: ["GET", "PUT", "DELETE", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
